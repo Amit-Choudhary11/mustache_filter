08ae3e4747177623942fe9a6ea00099687f6883c
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-09-10 19:11:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(640,480);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+
+
+function draw(){
+
+}
+
+
+function take_snapshot(){
+    save('filtered_image.png');
+}
+
