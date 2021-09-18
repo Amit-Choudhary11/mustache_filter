fb8ce76a90daec5195ef139d8903a979d9e4510d
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-09-18 08:43:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
-function preload(){
+noseX="";
+noseY="";
 
+function preload(){
+    mustache= loadImage("m.png");
 }
 
 function setup(){
-    canvas= createCanvas(640,480);
-    canvas.center();
+    canvas= createCanvas(390,300);
+    canvas.position(560,220);
     video= createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
-}
+    
+    poseNet= ml5.poseNet(video,modelLoded);
 
-
-
-function draw(){
-
-}
-
-
-function take_snapshot(){
-    save('filtered_image.png');
-}
-
+    poseNet.on("pose", getResult);
+    }
+    
+    function draw(){
+    image(video,0,0,390,300);
+    
+    
+    image(mustache,noseX + 25,noseY + 1,60,40);
+    
+    }
+    
+    function take_snapshot(){
+        save("filter.png");
+    }
+    
+    function modelLoded(){
+        console.log("model loaded");
+    }
+    
+    function getResult(results){
+        if(results.length > 0){
+          console.log(results);  
+    
+        noseX= results[0].pose.nose.x;
+        console.log(noseX);
+    
+        noseY= results[0].pose.nose.y;
+        console.log(noseY);
+        }
+    }
